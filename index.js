318d14dbc85b13325483096c1e1b1f6adc3ccb54
mbxisbankai <roy.mbui@student.moringaschool.com>
2025-03-18 11:31:24 +0300

    Sending Data With Fetch Lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(name, email){
+    return fetch('http://localhost:3000/users',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                'name': `${name}`,
+                'email': `${email}`
+            })
+            })
+            .then(res => res.json())
+            .then(user => {
+                const body = document.querySelector('body');
+                const paragraph = document.createElement('p');
+                paragraph.textContent = `${user.id}`;
+                body.appendChild(paragraph);
+            })
+            .catch(error => {
+                const body = document.querySelector('body');
+                const errorMessage = document.createElement('p');
+                errorMessage.textContent = `${error.message}`;
+                body.appendChild(errorMessage);
+            })
+}
